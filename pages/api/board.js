6a449d1188641a8700806575dd2560cf045f94da
Sam Poder <39828164+sampoder@users.noreply.github.com>
2022-08-05 14:16:08 -0700

    Update board.js

--- pages/api/board.js
+++ pages/api/board.js
@@ -1,11 +1,6 @@
 import prisma from '../../lib/prisma'
-const Mux = require('@mux/mux-node')
 
 export default async function handler(req, res) {
-  const { Video, Data } = new Mux(
-    process.env.MUX_TOKEN_ID,
-    process.env.MUX_TOKEN_SECRET
-  )
   let data = await prisma.account.findMany({
     include: {
       updates: {
@@ -15,22 +10,13 @@ export default async function handler(req, res) {
       }
     }
   })
-  let streams = await Video.LiveStreams.list()
-  let streamObject = {}
-  streams.map(stream => {
-    streamObject[stream.id] = stream
-  })
   data = data.map(x => ({
     ...x,
-    updates: [x.updates[0]],
-    stream: streamObject[x.stream]
+    updates: [x.updates[0]]
   }))
   data = data.map(x => ({
     username: x.username,
-    url:
-      x.stream?.status == 'active'
-        ? `https://stream.mux.com/${x.stream?.playback_ids[0].id}.m3u8`
-        : x.updates[0]?.attachments[0]
+    url: x.updates[0]?.attachments[0]
   }))
   res.json(data)
 }
