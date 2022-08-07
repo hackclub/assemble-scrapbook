8a10fba4484b36c9844f72d6faa00be17e8d5f8a
Max Wofford <max@maxwofford.com>
2022-08-06 22:42:34 -0700

    Swap to no cache url

--- pages/api/print/[id].js
+++ pages/api/print/[id].js
@@ -5,7 +5,8 @@ const CHANNEL = 'C03SJ0S42Q4'
 
 export default async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
-  const printFile = `https://scrapbook.assemble.hackclub.com/api/numbered-post-image/${req.query.id}`
+  const printFile = `http://forest.maxwofford.com/screenshot?id=${req.query.id}`
+  // const printFile = `https://scrapbook.assemble.hackclub.com/api/numbered-post-image/${req.query.id}`
   const result = await fetch('https://slack.com/api/chat.postMessage', {
     method: 'POST',
     headers: {
@@ -14,7 +15,7 @@ export default async (req, res) => {
     },
     body: JSON.stringify({
       channel: CHANNEL,
-      text: `please print this file and react once it's finished: ${printFile}`
+      text: `please confirm this file has printed and react once verified: ${printFile}`
     })
   })
   res.json({ success: true })
